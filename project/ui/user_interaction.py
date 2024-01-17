63d1301c9715ea011735d94d5a273cc225c99075
idan <idan31387@gmail.com>
2024-01-17 23:06:49 +0200

    .

---
+++ project/ui/user_interaction.py
@@ -0,0 +1,5 @@
+name = input('Hello, whats your name? ')
+board_size = int(input(f'please choose board size {name} '))
+number_of_mines = int(input(f'{name}, for board size {board_size}, choose number of mines to allocate: '))
+
+
